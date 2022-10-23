97005a1778b002f15e37dd34309ee09be0d64d19
DevoTrix <71916304+DevoTrix@users.noreply.github.com>
2022-10-23 10:00:39 -0800

    Update jchan443_lab4_part2.cpp

--- jchan443_lab4_part2.cpp
+++ jchan443_lab4_part2.cpp
@@ -2,12 +2,19 @@
 // functionality using the Arduino API. In contrast the zyBook,
 // the Arduino API does not provide the programmer with direct
 // access to Timer Interrupt functions. 
-const unsigned long period = 500; 
+/* Name: Joseph Chang
+   Email: jchan443@ucr.edu
+   I acknowledge all content contained herein, excluding template or example code, is my own original work.
+   Demo Vid: https://youtu.be/9ZYonyaOEyY
+*/
+const unsigned long period = 100; 
 unsigned long lastRan;
 int val = 0;
 const int b_size = 7;
 const int b[b_size] = {2, 3, 4, 5, 9, 10, 11};
 int t = 1;
+int p = 0;
+int k = 0;
 void setup(){
 
   // Default Arduino Function
@@ -33,16 +40,24 @@ void Tick(){
     switch(state){
       case INIT:  
         state = S0;  
-        break;
+        break;  
       case S0:
         val = analogRead(A0);
-        val = map(val, 0, 1021, 100, 500);
-        period = val;
-        t = t++;
-        if(t > 4){
-            t = 1;
+        val = map(val, 0, 1021, 1, 5);
+        p = val;    
+        if(k >=p){
+          t = t+1;
+          if(t > 4){
+              t = 1;
+          }          
+          k = 0;
+          state = S0;
         }
-        state = S0;
+        else{
+          k = k + 1;
+          state = S0;      
+        }
+        
         break;
       default: // should never get here
         break;
@@ -54,9 +69,9 @@ void Tick(){
         break;
       case S0:
         for(int i = 0; i < 4; i++){
-            digitalWrite(B[i], LOW);
+            digitalWrite(b[i], LOW);
         }
-        digitalWrite(B[ t - 1 ], HIGH);
+        digitalWrite(b[ t - 1 ], HIGH);
         break;
      default: // should never get here
         break;
