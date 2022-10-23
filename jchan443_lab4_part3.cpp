a6559362cee0d148d03e755b8830fed25bcc1991
DevoTrix <71916304+DevoTrix@users.noreply.github.com>
2022-10-23 10:26:18 -0800

    Update jchan443_lab4_part3.cpp

--- jchan443_lab4_part3.cpp
+++ jchan443_lab4_part3.cpp
@@ -2,11 +2,19 @@
 // functionality using the Arduino API. In contrast the zyBook,
 // the Arduino API does not provide the programmer with direct
 // access to Timer Interrupt functions. 
+/* Name: Joseph Chang
+   Email: jchan443@ucr.edu
+   I acknowledge all content contained herein, excluding template or example code, is my own original work.
+   Demo Vid: https://youtu.be/OEqI4-6tmNs
+*/
 const unsigned long period = 500; 
 unsigned long lastRan;
 
 const int b_size = 7;
 const int b[b_size] = {2, 3, 4, 5, 9, 10, 11};
+int rlp = 11;
+int blp = 9;
+int glp = 10;
 int t = 1;
 int val = 0;
 int nval = 0;
@@ -23,11 +31,14 @@ void setup(){
     {
         pinMode(b[i], OUTPUT);
     }
+  pinMode(rlp, OUTPUT);
+  pinMode(blp, OUTPUT);
+  pinMode(glp, OUTPUT);
     Serial.begin(9600);
     lastRan = 0;
 }
 int getBit(int x, int k){
-    return ((x& *(0x01 << k)) !=0);
+    return ((x & (0x01 << k)) !=0);
 }
 
 enum States{INIT, S0} state = INIT;
@@ -45,14 +56,27 @@ void Tick(){
         break;
       case S0:
         val = analogRead(A1);
-        val = map(val, 0, 160, 0, 15);
+        Serial.println(val);
+        val = map(val, 58, 750, 0, 15);
+        Serial.println(val);
         nval = analogRead(A0);
         //int rval = nval;
         int bval = nval;
         int gval = nval;
+        // Serial.println(bval);
         rmap = 255;
-        bmap = map(bval, 0, 500, 0, 255);
-        gmap = map(gval, 500, 1026, 0, 255);
+        if(bval > 50) {
+          bmap = map(bval, 70, 570, 0, 255);
+        }
+        else{
+          bmap = 0;
+        }
+        if(gval >=600){
+          gmap = map(gval, 500, 1026, 0, 255);
+        }
+        else{
+          gmap = 0;
+        }
         state = S0;
         break;
       default: // should never get here
@@ -65,17 +89,17 @@ void Tick(){
         break;
       case S0:
         for(int i = 0; i < 4; i++){
-            digitalWrite(B[i], LOW);
+            digitalWrite(b[i], LOW);
         }
         for(int i = 0; i < 4; i++){
-            if(getBit(val, i)){
-                digitalWrite(B[i], HIGH);
+            if(getBit(val, i)==1){
+                digitalWrite(b[i], HIGH);
             }
             else{
-                digitalWrite(B[i], LOW);
+                digitalWrite(b[i], LOW);
             }
         }
-        analogWrite(rlp, rmap);
+        analogWrite(rlp, 255);
         analogWrite(blp, bmap);
         analogWrite(glp, gmap);
         break;
